16ea14df823bbf3b876ed5b7090a2c80871eea16
jresetar <jozef.resetar@nokia.com>
2017-03-22 13:05:57 +0100

    fixed importing py3compat

--- src/scapy/layers/common.py
+++ src/scapy/layers/common.py
@@ -20,7 +20,7 @@
 Common constants and global variables.
 """
 import logging
-from paramiko.py3compat import byte_chr, PY2, bytes_types, string_types, b, long
+from py3compat import byte_chr, PY2, bytes_types, string_types, b, long
 
 MSG_DISCONNECT, MSG_IGNORE, MSG_UNIMPLEMENTED, MSG_DEBUG, MSG_SERVICE_REQUEST, \
     MSG_SERVICE_ACCEPT = range(1, 7)
